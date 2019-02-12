1188e7130daaae9e868fa486323ce0ace8e91e1f
Patrick Assoa <kanian77@gmail.com>
2019-02-12 16:01:18 +0000

    a few comments+readme

--- src/Exceptions/DependencyIsNotInstantiableException.php
+++ src/Exceptions/DependencyIsNotInstantiableException.php
@@ -9,9 +9,7 @@ class DependencyIsNotInstantiableException extends Exception implements Containe
 
     public function __construct($dependency, $code = 0, Exception $previous = null)
     {
-        // some code
         $message = "Dependency {$dependency} is not instantiable";
-        // make sure everything is assigned properly
         parent::__construct($message, $code, $previous);
     }
 }
